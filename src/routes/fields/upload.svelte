e7188771c5a92b58d6fea7ffc1989f315063feb9
Andrew Balmos <andrew@balmos.org>
2021-10-28 14:07:58 -0400

    Some  upload screen improvements
    Signed-off-by: Andrew Balmos <andrew@balmos.org>

--- src/routes/fields/upload.svelte
+++ src/routes/fields/upload.svelte
@@ -1,7 +1,7 @@
 <script context="module" lang="ts">
   // TODO: Is this the right way to init this?
   import init from '$lib/geo-utils/geo-utils';
-  import CheckmarkIcon from '$lib/icons/CheckmarkIcon.svelte';
+  // import CheckmarkIcon from '$lib/icons/CheckmarkIcon.svelte';
   import { goto } from '$app/navigation';
 
   export async function load() {
@@ -15,6 +15,8 @@
   import { getFeaturesFromShape } from '$lib/geo-utils/geo-utils';
   import FieldCard from '$lib/components/FieldCard.svelte';
   import { Field } from '$lib/db';
+  import Leaflet from '$lib/leaflet/Leaflet.svelte';
+  import GeoJson from '$lib/leaflet/GeoJson.svelte';
 
   let fieldNameKey = '';
   let fields: Array<{ checked: boolean; field: Field }> = [];
@@ -72,48 +74,85 @@
   {:else}
     <h1>One more step</h1>
     <p>I need a little help. Can you tell me which is the field name?</p>
-    <label>
-      <span>Field name</span>
-      <select bind:value={fieldNameKey}>
-        <option selected disabled hidden />
-        {#each Object.keys(fields[0]?.field.geojson.properties || {}) as key}
-          <option>{key}</option>
-        {/each}
-      </select>
-    </label>
+    <span>Field name</span>
+    <select
+      class="select selected-bordered select-error w-full max-w-xs"
+      bind:value={fieldNameKey}
+    >
+      <option selected disabled>Select field name</option>
+      {#each Object.keys(fields[0]?.field.geojson.properties || {}) as key}
+        <option>{key}</option>
+      {/each}
+    </select>
 
     <h1>Which fields would you like to import?</h1>
 
-    <div class="card-grid">
-      {#each fields as field}
-        <label class="floating relative">
-          <input
-            type="checkbox"
-            on:click={() => (field.checked = !field.checked)}
-          />
-          <CheckmarkIcon
-            class="absolute w-8 h-8 inset-1.5 z-500 text-gray-100 "
-            checked={field.checked}
-          />
-          <FieldCard field={field.field} />
-        </label>
-      {/each}
+    <div class="overflow-x-auto">
+      <table class="table table-fixed table-zebra w-full">
+        <thead>
+          <tr>
+            <th class="w-16" />
+            <th class="w-24 text-center">Field</th>
+            <th class="w-auto">Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each fields as field}
+            <tr
+              class:active={field.checked}
+              on:click={() => (field.checked = !field.checked)}
+            >
+              <th>
+                <label>
+                  <input
+                    type="checkbox"
+                    class="checkbox checkbox-lg"
+                    checked={field.checked}
+                  />
+                </label>
+              </th>
+              <td>
+                <div class="flex items-left space-x-3">
+                  <div class="avatar">
+                    <div class="w-16 h-16 mask mask-squircle">
+                      <Leaflet
+                        class="h-full"
+                        zoomControl={false}
+                        dragable={false}
+                        zoomable={false}
+                      >
+                        <GeoJson geojson={field.field.geojson} zoomTo={true} />
+                      </Leaflet>
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                {#if field.field.name}
+                  <strong>
+                    {field.field.name}
+                  </strong>
+                {:else}
+                  <i>Please select name field above...</i>
+                {/if}
+                <div class="text-sm opacity-50">
+                  {field.field.area.toFixed(1)} ac.
+                </div>
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
     </div>
 
-    <label class="submit">
-      <button type="submit" on:click={handleSubmit}>
-        Import {fields.filter((f) => f.checked).length} Fields
-      </button>
-    </label>
+    <div class="relative w-full h-32">
+      <div class="absolute inset-x-16 bottom-0">
+        <label class="submit ">
+          <button type="submit" on:click={handleSubmit}>
+            Import {fields.filter((f) => f.checked).length} Fields
+          </button>
+        </label>
+      </div>
+    </div>
   {/if}
 </div>
-
-<style type="postcss">
-  label.floating input {
-    @apply absolute w-full h-full opacity-0 inset-0;
-  }
-
-  label.submit {
-    @apply mb-20;
-  }
-</style>
